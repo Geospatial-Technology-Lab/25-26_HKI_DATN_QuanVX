2dd47e51e6f5ecf597e50bce2e90299ed3ce8f82
HiImKaii <smsy212608@gmail.com>
2025-12-02 14:53:25 +0700

    bfe

--- ml/config/model_params.py
+++ ml/config/model_params.py
@@ -2,14 +2,14 @@ RF_PARAM_RANGES = {
     'n_estimators': {'type': 'int', 'min': 50, 'max': 1000},
     'max_features': {'type': 'int', 'min': 1, 'max': 13},
     'min_samples_leaf': {'type': 'int', 'min': 1, 'max': 20},
-    'max_leaf_nodes': {'type': 'int', 'min': 50, 'max': 20000}
+    'max_leaf_nodes': {'type': 'int', 'min': 50, 'max': 1000}
 }
 
 XGB_PARAM_RANGES = {
-    'n_estimators': {'type': 'int', 'min': 100, 'max': 1500},
+    'n_estimators': {'type': 'int', 'min': 50, 'max': 1000},
     'learning_rate': {'type': 'float', 'min': 0.001, 'max': 0.5},
     'subsample': {'type': 'float', 'min': 0.5, 'max': 1.0},
-    'max_leaves': {'type': 'int', 'min': 50, 'max': 20000}
+    'max_leaves': {'type': 'int', 'min': 50, 'max': 1000}
 }
 
 SVM_PARAM_RANGES = {
